1947c6eecbd9221b5bb731c8441929b014aa302a
Miguelangel Bravo Migone <mbmigone@outlook.com>
2019-01-05 19:59:09 -0300

    Formulario principal

---
+++ FreeMe/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace FreeMe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbHead = new System.Windows.Forms.Label();
+            this.lbRemain = new System.Windows.Forms.Label();
+            this.lbMensaje = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cerrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbHead
+            // 
+            this.lbHead.AutoSize = true;
+            this.lbHead.Location = new System.Drawing.Point(12, 9);
+            this.lbHead.Name = "lbHead";
+            this.lbHead.Size = new System.Drawing.Size(74, 13);
+            this.lbHead.TabIndex = 0;
+            this.lbHead.Text = "Todavía falta:";
+            // 
+            // lbRemain
+            // 
+            this.lbRemain.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRemain.Location = new System.Drawing.Point(12, 22);
+            this.lbRemain.Name = "lbRemain";
+            this.lbRemain.Size = new System.Drawing.Size(236, 63);
+            this.lbRemain.TabIndex = 1;
+            this.lbRemain.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbMensaje
+            // 
+            this.lbMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMensaje.ForeColor = System.Drawing.Color.DarkRed;
+            this.lbMensaje.Location = new System.Drawing.Point(12, 85);
+            this.lbMensaje.Name = "lbMensaje";
+            this.lbMensaje.Size = new System.Drawing.Size(237, 133);
+            this.lbMensaje.TabIndex = 2;
+            this.lbMensaje.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cerrarToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(107, 26);
+            // 
+            // cerrarToolStripMenuItem
+            // 
+            this.cerrarToolStripMenuItem.Name = "cerrarToolStripMenuItem";
+            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
+            this.cerrarToolStripMenuItem.Text = "Cerrar";
+            this.cerrarToolStripMenuItem.Click += new System.EventHandler(this.cerrarToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 227);
+            this.Controls.Add(this.lbMensaje);
+            this.Controls.Add(this.lbRemain);
+            this.Controls.Add(this.lbHead);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FreeMe v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbHead;
+        private System.Windows.Forms.Label lbRemain;
+        private System.Windows.Forms.Label lbMensaje;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cerrarToolStripMenuItem;
+    }
+}
+
---
+++ FreeMe/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+
+namespace FreeMe
+{
+    
+    public partial class Form1 : Form
+    {
+        string ajá = "así que decidiste crackerme? No me veas! D:";
+        string ejé = "bueno, si llegaste hasta aquí el código fuente está en github";
+        string ijí = "https://github.com/mbmigone/FreeMe";
+        //variables de control de flujo. Que los popup se ejecuten una sola vez.
+        bool executed1, executed2, executed3, executed4, executed5; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan Span = new TimeSpan(18, 15, 0); //Hora de salida = 18:15
+            DateTime hoy = DateTime.Now; //La fecha/hora actuales
+            TimeSpan ahora = hoy.TimeOfDay; //La hora actual
+            TimeSpan result = Span - ahora; // Hora de salida - Hora actual = Tiempo restante para irse
+
+            lbRemain.Text = result.ToString(@"h\:mm\:ss"); //result se convierte a String para mostrarlo en el Form1
+
+            // Tooltip del icono de la barra de sistema. Mouse Hover muestra el tiempo restante sin abrir el programa.
+            notifyIcon1.Text = "FreeMe: " + lbRemain.Text;
+            
+            //Si se llego a la hora de salida (Hora de salida - Hora actual = 0).
+            if (result.Hours <= 0 && result.Minutes == 0 && result.Seconds == 0 && !executed1)
+            {
+                try
+                {
+                    SoundPlayer sndPlayer = new SoundPlayer(FreeMe.Properties.Resources.ibelieve);
+                    sndPlayer.Play(); //Toca I Believe I can fly...
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error message - " + ex.Message); //por si acaso no mas.
+                }
+                //Trae la ventana del programa al frente
+                Show();
+                WindowState = FormWindowState.Normal;
+                executed1 = true;
+            }
+
+            //Una hora antes de la salida avisa que queda poco.
+            if (result.Hours == 1 && result.Minutes == 0 && result.Seconds == 0 && !executed2)
+            {
+                notifyIcon1.BalloonTipText = "Ya queda poco :D";
+                notifyIcon1.ShowBalloonTip(1000);
+                executed2 = true;
+            }
+
+            //15 minutos antes de la salida avisa para ir guardando y cerrando.
+            if(result.Hours == 0 && result.Minutes == 15 && result.Seconds == 0 && !executed3)
+            {
+                notifyIcon1.BalloonTipText = "Anda arreglando tus weaitas y apagando";
+                notifyIcon1.ShowBalloonTip(1000);
+                executed3 = true;
+            }
+
+            //Despues de pasada la hora de salida se muestra el tiempo adicional que se ha estado.
+            if (ahora.Hours >= 18 && ahora.Minutes >= 15 && !executed4)
+            {
+                lbHead.Text = "Tiempo adicional en la oficina:";
+                lbRemain.ForeColor = Color.DarkRed;
+                lbMensaje.Text = "Qué estai haciendo acá todavia? FUERA";
+                executed4 = true;
+            }
+
+            //Despues de pasada la hora de salida se muestra el tiempo adicional que se ha estado.
+            if (ahora.Hours >= 20 && ahora.Minutes >= 00 && !executed5)
+            {
+                lbHead.Text = "Tiempo adicional en la oficina:";
+                lbRemain.ForeColor = Color.DarkRed;
+                lbMensaje.Text = "Todavia por acá? en serio? -_-";
+                executed5 = true;
+            }
+        }
+
+        //Al momento de cargar la ventana principal del programa:
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Inicia el timer.
+            timer.Start();
+           
+            //Setea algunas cosas de mensajes
+            notifyIcon1.BalloonTipTitle = "FreeMe";
+            notifyIcon1.BalloonTipText = "Yo me quedo por aquí...";
+            notifyIcon1.Text = "FreeMe";
+
+            try
+            {
+                SoundPlayer sndPlayer = new SoundPlayer(FreeMe.Properties.Resources.hellodark);
+                sndPlayer.Play(); //Toca Hello Darkness my old friend...
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error message - " + ex.Message);
+            }
+        }
+
+        //Doble clic en el icono de sistema
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //Trae el programa al frente
+            Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        //Cambio de tamaño del Form1
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if(WindowState == FormWindowState.Minimized)
+            {
+                //Si se trata de un minimizado oculta la ventana en la barra de sistema. 
+                Hide();
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(1000);
+            }
+            else if(FormWindowState.Normal == WindowState)
+            {
+                //Si fue un normalizado de ventana, oculta el icono de sistema
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void cerrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Clic derecho sobre el icono de sistema contiene una opción para cerrar el programa
+            this.Close();
+        }
+    }
+}
